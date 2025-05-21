4cd33ddc32cacd7544e455604bbededa1e321b5f
nicscl <nicholassclima@gmail.com>
2025-05-21 15:19:02 -0300

    Refactor: Update Balancer price helper to V3 API
    - Refactored helpers/balancer_price.py to use Balancer V3 Vault API (getPoolTokenInfo).
    - Added BALANCER_VAULT_V3_ABI to config/abis/balancer.py.
    - Updated config/abis/__init__.py to export BALANCER_VAULT_V3_ABI.
    - Removed logging from balancer_price.py.
    - Ensured correct fallback vault address is used.

--- config/abis/erc20.py
+++ config/abis/erc20.py
@@ -6,6 +6,16 @@ Contains the standard ERC20 interface functions for balance, approval, and allow
 """
 
 ERC20_ABI = [
+    # Standard ERC-20 metadata view (needed by helpers.balancer_price)
+    {
+        "constant": True,
+        "inputs": [],
+        "name": "decimals",
+        "outputs": [{"name": "", "type": "uint8"}],
+        "payable": False,
+        "stateMutability": "view",
+        "type": "function",
+    },
     {"constant": True, "inputs": [{"name": "owner", "type": "address"}], "name": "balanceOf", "outputs": [{"name": "", "type": "uint256"}], "payable": False, "stateMutability": "view", "type": "function"},
     {"constant": False, "inputs": [{"name": "spender", "type": "address"}, {"name": "amount", "type": "uint256"}], "name": "approve", "outputs": [{"name": "", "type": "bool"}], "payable": False, "stateMutability": "nonpayable", "type": "function"},
     {"constant": True, "inputs": [{"name": "owner", "type": "address"}, {"name": "spender", "type": "address"}], "name": "allowance", "outputs": [{"name": "", "type": "uint256"}], "payable": False, "stateMutability": "view", "type": "function"}
